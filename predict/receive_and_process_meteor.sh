8d2e3c28d8cf328db81bcb18a709a67d02a87b0b
guidocioni <guidocioni@gmail.com>
2022-03-31 11:45:14 +0000

    Corrected handling of multiple folders

--- predict/receive_and_process_meteor.sh
+++ predict/receive_and_process_meteor.sh
@@ -8,49 +8,52 @@
 
 cd /home/ubuntu/weather_sat
 
-# timeout $6 predict/rtlsdr_m2_lrpt_rx.py $1 $2 raw_files/$3
-timeout $6 predict/airspy_m2_lrpt_rx.py $1 $2 raw_files/$3
+timeout $6 predict/rtlsdr_m2_lrpt_rx.py $1 $2 $3
+# timeout $6 predict/airspy_m2_lrpt_rx.py $1 $2 $3
 
 # Winter
-#medet/medet_arm raw_files/${3}.s raw_files/$3 -r 68 -g 65 -b 64 -na -S
+#medet/medet_arm ${3}.s $3 -r 68 -g 65 -b 64 -na -S
 
 # Summer
-medet/medet_arm raw_files/${3}.s images/$3 -r 66 -g 65 -b 64 -na -S
+medet/medet_arm ${3}.s $3 -r 66 -g 65 -b 64 -na -S
 
-rm raw_files/${3}.s
+rm ${3}.s
 
-if [ -f "images/${3}_0.bmp" ]; then
+if [ -f "${3}_0.bmp" ]; then
 	dte=`date +%H`
 	# Winter
-	#convert images/${3}_1.bmp images/${3}_1.bmp images/${3}_0.bmp -combine -set colorspace sRGB images/${3}.bmp
-    #convertimages/ ${3}_2.bmp images/${3}_2.bmp images/${3}_2.bmp -combine -set colorspace sRGB -negate images/${3}_ir.bmp
+	#convert ${3}_1.bmp ${3}_1.bmp ${3}_0.bmp -combine -set colorspace sRGB ${3}.bmp
+    #convert ${3}_2.bmp ${3}_2.bmp ${3}_2.bmp -combine -set colorspace sRGB -negate ${3}_ir.bmp
 
 	# Summer
-	convert images/${3}_2.bmp images/${3}_1.bmp images/${3}_0.bmp -combine -set colorspace sRGB images/${3}.bmp
+	convert ${3}_2.bmp ${3}_1.bmp ${3}_0.bmp -combine -set colorspace sRGB ${3}.bmp
 
-	meteor_rectify/rectify.py images/${3}.bmp
+	meteor_rectify/rectify.py ${3}.bmp
 
 	# Winter only
-	#meteor_rectify/rectify.py images/${3}_ir.bmp
+	#meteor_rectify/rectify.py ${3}_ir.bmp
 
 	# Rotate evening images 180 degrees
 	if [ $dte -lt 13 ]; then
-		convert images/${3}-rectified.png -normalize -quality 90 images/$3.jpg
+		convert ${3}-rectified.png -normalize -quality 90 $3.jpg
         # Winter only
-		#convert images/${3}_ir-rectified.png -normalize -quality 90 images/${3}_ir.jpg
+		#convert ${3}_ir-rectified.png -normalize -quality 90 ${3}_ir.jpg
 	else
-		convert images/${3}-rectified.png -rotate 180 -normalize -quality 90 images/$3.jpg
+		convert ${3}-rectified.png -rotate 180 -normalize -quality 90 $3.jpg
 		# Winter only
-		#convert images/${3}_ir-rectified.png -rotate 180 -normalize -quality 90 images/${3}_ir.jpg
+		#convert ${3}_ir-rectified.png -rotate 180 -normalize -quality 90 ${3}_ir.jpg
 	fi
 
-	rm images/$3.bmp
-	rm images/${3}_0.bmp
-	rm images/${3}_1.bmp
-	rm images/${3}_2.bmp
-	# rm images/${3}-rectified.png
+	rm $3.bmp
+	rm ${3}_0.bmp
+	rm ${3}_1.bmp
+	rm ${3}_2.bmp
+	# rm ${3}-rectified.png
+
+	mv ${3}-rectified.png images/
+	mv $3.jpg images/
 
 	# Winter only
-	#rm images/${3}_ir.bmp
-	#rm images/${3}_ir-rectified.png
+	#rm ${3}_ir.bmp
+	#rm ${3}_ir-rectified.png
 fi
--- predict/receive_and_process_satellite.sh
+++ predict/receive_and_process_satellite.sh
@@ -7,15 +7,20 @@
 # $5 = EPOC start time
 # $6 = Time to capture
 
-timeout $6 rtl_fm -d 00000978 -f ${2}M -s 48k -g 40 -p 5 -E dc -A fast -F 9 - | sox -t raw -r 48000 -es -b16 -c1 -V1 - raw_files/$3.wav rate 11025 
+cd /home/ubuntu/weather_sat
+
+timeout $6 rtl_fm -d 00000978 -f ${2}M -s 48k -g 40 -p 5 -E dc -A fast -F 9 - | sox -t raw -r 48000 -es -b16 -c1 -V1 - $3.wav rate 11025 
 
 PassStart=`expr $5 + 90`
 
-if [ -e raw_files/$3.wav ]
+if [ -e $3.wav ]
   then
-    /usr/local/bin/wxmap -T "${1}" -H $4 -p 0 -l 0 -o $PassStart images/${3}-map.png
+    /usr/local/bin/wxmap -T "${1}" -H $4 -p 0 -l 0 -o $PassStart ${3}-map.png
 
-    /usr/local/bin/wxtoimg -m images/${3}-map.png -e MSA raw_files/$3.wav images/$3.png
+    /usr/local/bin/wxtoimg -m ${3}-map.png -e MSA $3.wav $3.png
 
-    rm images/${3}-map.png
+    rm ${3}-map.png
 fi
+
+mv $3.wav raw_files/
+mv $3.png images/
